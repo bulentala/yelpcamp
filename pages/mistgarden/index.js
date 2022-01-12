aa128c727aad67b37f206f448eb2e657f482e484
Bülent ALA <46373855+bulentala@users.noreply.github.com>
2022-01-12 23:56:32 +0300

    ok

--- pages/mistgarden/index.js
+++ pages/mistgarden/index.js
@@ -102,18 +102,14 @@ const Index = () => {
           <p className="text-[14px] text-[#475F45]">View all posts</p>
         </div>
         <div className="lg:grid lg:grid-cols-2">
-          <div className="p-6 text-center md:flex lg:flex-col md:text-left">
-            <div className="relative aspect-2">
-              <Image
-                className="absolute object-cover "
-                alt=""
-                src="https://dummyimage.com/520x333"
-                width="520"
-                height="333"
-              />
-            </div>
-
-            <div className="px-6">
+          <div className="p-4 text-center md:text-left md:flex lg:block">
+            <Image
+              alt=""
+              src="https://dummyimage.com/520x333"
+              width="520"
+              height="333"
+            />
+            <div className="pl-4 lg:pl-0">
               <p className="text-[23px] font-serif text-[#475F45]">
                 Plants Around Us
               </p>
@@ -127,19 +123,15 @@ const Index = () => {
               </p>
             </div>
           </div>
-          <div className="text-center md:text-left">
-            <div className="p-6 border-b border-gray-200 md:flex">
-              <div className="relative aspect2">
-                <Image
-                  className="absolute object-cover"
-                  alt=""
-                  src="https://dummyimage.com/236x151"
-                  width="236"
-                  height="151"
-                />
-              </div>
-
-              <div className="px-6">
+          <div>
+            <div className="p-4 text-center border-b border-gray-200 md:text-left md:flex">
+              <Image
+                alt=""
+                src="https://dummyimage.com/236x151"
+                width="236"
+                height="151"
+              />
+              <div className="pl-4 ">
                 <p className="text-[23px] font-serif text-[#475F45]">
                   Lush Gardens
                 </p>
@@ -152,50 +144,43 @@ const Index = () => {
                 </p>
               </div>
             </div>
-            <div className="p-6 border-b border-gray-200 md:flex">
-              <div className="relative aspect2">
-                <Image
-                  className="absolute object-cover"
-                  alt=""
-                  src="https://dummyimage.com/236x151"
-                  width="236"
-                  height="151"
-                />
-              </div>
-              <div className="px-6">
+            <div className="p-4 text-center border-b border-gray-200 md:text-left md:flex">
+              <Image
+                alt=""
+                src="https://dummyimage.com/236x151"
+                width="236"
+                height="151"
+              />
+              <div className="pl-4 ">
                 <p className="text-[23px] font-serif text-[#475F45]">
-                  Exotic Nature
+                  Lush Gardens
                 </p>
                 <p className="text-[20px] text-[#414840]">
                   Lorem ipsum dolor sit amet, consectetur donec adipiscing elit.
                   Tincidunt facilisis nunc lorem
                 </p>
                 <p className="text-[#414840] text-[16px] italic">
-                  November 11, 2021
+                  December 16, 2021
                 </p>
               </div>
             </div>
-            <div className="p-6 border-b border-gray-200 md:flex">
-              <div className="relative aspect2">
-                <Image
-                  className="absolute object-cover"
-                  alt=""
-                  src="https://dummyimage.com/236x151"
-                  width="236"
-                  height="151"
-                />
-              </div>
-
-              <div className="px-6">
+            <div className="p-4 text-center border-b border-gray-200 md:text-left md:flex">
+              <Image
+                alt=""
+                src="https://dummyimage.com/236x151"
+                width="236"
+                height="151"
+              />
+              <div className="pl-4 ">
                 <p className="text-[23px] font-serif text-[#475F45]">
-                  It Starts with Soil
+                  Lush Gardens
                 </p>
                 <p className="text-[20px] text-[#414840]">
                   Lorem ipsum dolor sit amet, consectetur donec adipiscing elit.
                   Tincidunt facilisis nunc lorem
                 </p>
                 <p className="text-[#414840] text-[16px] italic">
-                  November 3, 2021
+                  December 16, 2021
                 </p>
               </div>
             </div>
--- pages/test3.js
+++ pages/test3.js
@@ -2,34 +2,85 @@ import Image from "next/image";
 
 const Test3 = () => {
   return (
-    <div className="max-w-5xl p-12 border-8 border-red-500 ">
-      <div className="grid grid-cols-2 grid-rows-2 gap-4">
+    <div className="lg:grid lg:grid-cols-2">
+      <div className="p-4 text-center md:text-left md:flex lg:block">
         <Image
           alt=""
-          src="https://dummyimage.com/534x354"
-          width="534"
-          height="354"
+          src="https://dummyimage.com/520x333"
+          width="520"
+          height="333"
         />
-
-        <div className="col-span-2">
+        <div className="pl-4 lg:pl-0">
+          <p className="text-[23px] font-serif text-[#475F45]">
+            Plants Around Us
+          </p>
+          <p className="text-[16px] text-[#414840]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt
+            facilisis nuncLorem ipsum dolor sit.Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit...
+          </p>
+          <p className="text-[#414840] text-[16px] italic">December 23, 2021</p>
+        </div>
+      </div>
+      <div>
+        <div className="p-4 text-center border-b border-gray-200 md:text-left md:flex">
           <Image
             alt=""
-            src="https://dummyimage.com/1040x409"
-            width="1040"
-            height="409"
+            src="https://dummyimage.com/236x151"
+            width="236"
+            height="151"
           />
+          <div className="pl-4 ">
+            <p className="text-[23px] font-serif text-[#475F45]">
+              Lush Gardens
+            </p>
+            <p className="text-[20px] text-[#414840]">
+              Lorem ipsum dolor sit amet, consectetur donec adipiscing elit.
+              Tincidunt facilisis nunc lorem
+            </p>
+            <p className="text-[#414840] text-[16px] italic">
+              December 16, 2021
+            </p>
+          </div>
         </div>
-        <div className="col-start-2 row-start-1 place-self-center">
-          <p className="text-[45px] font-serif ">
-            Discover yourself with nature
-          </p>
-          <p className="text-[20px] ">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt
-            facilisis nuncLorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Tincidunt facilisis nunc
-          </p>
-          <div className="text-[18px] border border-gray-500 w-max px-4 py-3  ">
-            Learn More
+        <div className="p-4 text-center border-b border-gray-200 md:text-left md:flex">
+          <Image
+            alt=""
+            src="https://dummyimage.com/236x151"
+            width="236"
+            height="151"
+          />
+          <div className="pl-4 ">
+            <p className="text-[23px] font-serif text-[#475F45]">
+              Lush Gardens
+            </p>
+            <p className="text-[20px] text-[#414840]">
+              Lorem ipsum dolor sit amet, consectetur donec adipiscing elit.
+              Tincidunt facilisis nunc lorem
+            </p>
+            <p className="text-[#414840] text-[16px] italic">
+              December 16, 2021
+            </p>
+          </div>
+        </div>
+        <div className="p-4 text-center border-b border-gray-200 md:text-left md:flex">
+          <Image
+            alt=""
+            src="https://dummyimage.com/236x151"
+            width="236"
+            height="151"
+          />
+          <div className="pl-4 ">
+            <p className="text-[23px] font-serif text-[#475F45]">
+              Lush Gardens
+            </p>
+            <p className="text-[20px] text-[#414840]">
+              Lorem ipsum dolor sit amet, consectetur donec adipiscing elit.
+              Tincidunt facilisis nunc lorem
+            </p>
+            <p className="text-[#414840] text-[16px] italic">
+              December 16, 2021
+            </p>
           </div>
         </div>
       </div>
